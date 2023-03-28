6ee33ba8e809e0647086814b794ef489b640ea39
LauraCullenHill <laurahill207@hotmail.com>
2023-03-28 16:06:25 +0100

    testing functions added

--- shop.py
+++ shop.py
@@ -1,7 +1,8 @@
-#what is in the basket
 my_basket = {"apple": 0.50, "persian cat": 800, "orange": 0.50}
 
-#define the functions
+
+# define the functions
+
 def basket_total(basket):
     total = sum(basket.values())
     return total
@@ -28,24 +29,32 @@ def my_items(basket):
 
 
 def try_again():
-    counter = 0
-    retry = input("Do you have more money? y/n: ")
-    while retry == 'y' and counter < 3:
-        checkout()
-        counter += 1
-    if counter < 3:
-        print("Cannot exceed 3 attempts")
-        exit()
+    count = 0
+    while count < 3:
+        try:
+            retry = input("Do you have more money? y/n: ")
+            if retry == 'y':
+                count += 1
+                checkout()
+            else:
+                count += 1
+                break  # exit the inner while loop if the user chooses not to retry
+        except:
+            if count >= 3:
+                print("Cannot exceed 3 attempts")
+                break  # exit the outer while loop if the user exceeds 3 retries
+    return "End."
 
 
 # define the exceptions
+
 def in_budget(basket):
-    if my_budget() <= basket_total(basket):
+    if my_budget() < basket_total(basket):
         raise ValueError("You do not have enough money")
 
 
-def integer_validation(basket):
-    if not int:
+def integer_validation(value):
+    if not isinstance(value, int):
         raise ValueError("You must enter an integer")
 
 
@@ -56,16 +65,17 @@ def checkout():
         print(f"Welcome to the store \nYour total comes to: £{total}")
         if_exit()
         in_budget(my_basket)
-        integer_validation(my_basket)
+        integer_validation(my_budget())
     except Exception as exc:
         print(exc)
         try_again()
     else:
         total = basket_total(my_basket)
-        print(f"Please pay {total}")
+        print(f"Please pay £{total}")
         items = my_items(my_basket)
         print(f"Please take your {items}")
     finally:
         print('Program finished')
+
 
 checkout()
--- shop_testing.py
+++ shop_testing.py
@@ -1,22 +1,43 @@
-from unittest import mock
-from unittest import TestCase, main
-from .shop import , average_exam_score, increment_line_number
+from unittest import TestCase, main, mock
+
+# import the functions
+from shop import basket_total, my_budget, in_budget, integer_validation
 
 
-class TestIfExitFunction(TestCase):
+# define test data
+my_basket = {"apple": 0.50, "persian cat": 800, "orange": 0.50}
 
-    def test_odd_numbers(self):
-        expected = 'Red'
-        result = red_or_blue(num=3)
-        self.assertEqual(expected, result)
 
-    def test_even_greater_than_twenty(self):
-        expected = 'Blue'
-        result = red_or_blue(num=54)
-        self.assertEqual(expected, result)
+class TestBasket(TestCase):
+    # test the basket_total function
+    def test_basket_total(self):
+        expected_total = 801.0
+        assert basket_total(my_basket) == expected_total
 
-    def test_range_6_to_20(self):
-        expected = 'Red'
-        result = red_or_blue(num=12)
-        self.assertEqual(expected, result)
-#
+
+class TestMyBudget(TestCase):
+    # test the basket_total function
+    def test_my_budget(self):
+        self.assertTrue(my_budget() == 1000)
+
+
+class TestInBudget(TestCase):
+    # test the in_budget function with enough money
+    def test_in_budget_enough_money(self):
+        self.assertIsNone(in_budget(my_basket))
+
+    def test_in_budget_not_enough_money(self):
+        with self.assertRaises(ValueError) as test_budget:
+            in_budget({"apple": 1000})
+        self.assertEqual(str(test_budget.exception), "You do not have enough money")
+
+
+class TestIntegerValidation(TestCase):
+
+    # Test integer_validation function with a string
+    def test_integer_validation_with_string(self):
+        with self.assertRaises(ValueError) as context:
+            integer_validation("string")
+        self.assertEqual(str(context.exception), "You must enter an integer")
+
+
